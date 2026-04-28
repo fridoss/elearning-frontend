bb9e7bd8c957921953e1744b20929a80e4832270
fridoss <fridossfofana5@gmail.com>
2026-04-28 10:59:16 +0000

    Fix API URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://elearning-backend-yshd.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
